c6a3a367f13db57473a8cdd2fb9ef602df6b3dff
Kashin Aleksey <kashin.aleksey@gmail.com>
2024-09-02 20:21:16 +0300

    task4

--- _run.sh
+++ _run.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 python3 -m venv venv  # on Windows, use "python -m venv venv" instead
 . venv/bin/activate   # on Windows, use "venv\Scripts\activate" instead
 pip install -r requirements.txt
